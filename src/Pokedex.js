b217b54c0c1731a9090fe7bafcac31eb405f1a8d
Kate Moser <komoser@gmail.com>
2022-02-28 17:28:19 -0600

    initial files

---
+++ src/Pokedex.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function Pokedex(){
+    //takes props (array of objects => {id: 4, name"charmander", type: fire, base_experience: 62} )
+    //turn em each into a pokecard
+    //return that
+}
