fbfc8a1ff42cfe47591bcd4b25e116f20447ff12
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-02-28 16:18:30 -0800

    working pokedex. Completed code review. On further study now

---
+++ src/Pokecard.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** Creates a display card from arguments {id, name, type, base_exp} */
+function Pokecard({ id, name, type, base_exp }) {
+
+    const BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+    // CR: put a class name
+    return (
+        <div className="Pokecard">
+            <b>{name}</b>
+            <img src={`${BASE_URL}${id}.png`}></img>
+            <p>Type: {type}</p>
+            <p>EXP: {base_exp}</p>
+        </div>
+    );
+}
+
+export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,19 @@
 "use strict";
+import Pokecard from "./Pokecard";
 
-function Pokedex(){
+/** Creates */
+function Pokedex({ pokemon }){
     //takes props (array of objects => {id: 4, name"charmander", type: fire, base_experience: 62} )
-    //turn em each into a pokecard
-    //return that
+    return (<div className="Pokedex">
+
+        {pokemon.map(p => (
+            < Pokecard id = {p.id} 
+            name = {p.name} 
+            type = {p.type} 
+            base_exp={p.base_experience}/>
+        ))}
+
+    </div>);
 }
+// CR: explicity pass parameters from each component
+export default Pokedex;
